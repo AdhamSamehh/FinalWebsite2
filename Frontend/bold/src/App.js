78271c5de4eb5344c5ebe7511719efc81e848ab3
AdhamSamehh <an2300132@tkh.edu.eg>
2024-12-13 13:17:58 +0200

    Backend v3

--- Frontend/bold/src/App.js
+++ Frontend/bold/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cart from './components/Cart';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/cart" element={<Cart />} />
+          <Route path="/not-found" element={<NotFound />} />
           <Route path="/" exact element={<Home />} />
         </Routes>
       </BrowserRouter>
